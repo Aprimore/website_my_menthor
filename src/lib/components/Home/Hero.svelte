324e8d60122e596f43467bca8a825a84f52070ec
Gabriel Christo <gabrielchristomarques@gmail.com>
2024-09-17 02:39:24 -0300

    experiment

--- src/lib/components/Home/Hero.svelte
+++ src/lib/components/Home/Hero.svelte
@@ -13,7 +13,7 @@
 	// 	header_hero_3_webp,
 	// 	header_hero_webp
 	// } from '$lib';
-	import { header_hero_webp } from '$lib';
+	import { header_hero_webp, products_s3_bg_radial_svg } from '$lib';
 </script>
 
 <!-- <svelte:head>
@@ -23,8 +23,8 @@
 	<link rel="preload" as="image" href={header_hero_webp} />
 </svelte:head> -->
 
-<header in:fade={{ delay: 0, duration: 150, easing: cubicInOut }} class="hero-section bg-[#19182F]">
-	<div class="mx-auto px-4 flex h-[510px] max-sm:h-[400px] items-center relative">
+<header in:fade={{ delay: 0, duration: 150, easing: cubicInOut }}>
+	<div class="mx-auto px-4 flex h-[510px] max-sm:h-[400px] items-center bg-black">
 		<div
 			in:fade={{ delay: 200, duration: 150, easing: cubicInOut }}
 			class="mx-auto max-w-5xl text-center z-20"
@@ -41,14 +41,7 @@
 				{@html m.homepage_header_webhave()}
 			</p>
 
-			<button class="Exo">
-				<a
-					class="btn-demo bg-[#8abb3f] block w-full rounded-xl px-10 py-4 text-lg text-[#17193B] focus:ring sm:w-auto"
-					href="/company#contact"
-				>
-					{@html m.button_schedule_a_demo()}
-				</a>
-			</button>
+
 
 			<!-- srcset="
 				{header_hero_1_webp} 300w,
@@ -56,33 +49,12 @@
 				{header_hero_3_webp} 900w,
 				{header_hero_webp} 1200w,
 				" -->
-			<img
+			<!-- <img
 				src={header_hero_webp}
-				alt="Hero Header"
+				alt="My Menthor Header"
 				class="absolute inset-0 w-full h-full object-cover -z-10"
-			/>
+			/> -->
 		</div>
 	</div>
 </header>
 
-<style>
-	.btn-demo::after {
-		content: url('$lib/assets/svg/arrow_right_2.svg');
-		display: inline-block;
-		/* vertical-align: middle; */
-		margin-left: 10px;
-	}
-
-	h1.yellow-underline {
-		content: '';
-		background-image: url('$lib/assets/svg/linha_h_amarela4.svg');
-		background-repeat: no-repeat;
-		background-position: bottom right;
-		padding-bottom: 10px;
-	}
-	@media (max-width: 709px) {
-		h1.yellow-underline {
-			background-size: 100% auto;
-		}
-	}
-</style>
